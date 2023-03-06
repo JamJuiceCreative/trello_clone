0a759e18d1ebbda650b7611f4f4e406254845e49
JamJuiceCreative <jim@jamjuicecreative.com.au>
2023-03-06 14:25:58 +1000

    Controllers

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+
+
+db = SQLAlchemy()
+ma = Marshmallow()
+
+
+def create_app():
+    #using a list comprehension and multiple assignment
+    #to grab the environment variables we need
+
+    # Creating the flask app objects - This is the core of our app
+
+    app = Flask(__name__)
+
+    # configuring our app:
+    app.config.from_object("config.app_config")
+
+    # creating our database object! This allows us to use our ORM
+
+    db.init_app(app)
+    ma.init_app(app)
+
+    # import the controllers and activate the blueprints
+    from controllers import registerable_controllers
+
+    for controller in registerable_controllers:
+        app.register_blueprint(controller)
+    
+
+    return app
